291f72b902c36e5b2f63f683d173e7a4282d1bda
Luk-ESC <eschbacher.lukas@gmail.com>
2025-09-24 10:15:09 +0200

    fix(linking): don't hardcode the paths to C runtime objects

--- src/linking.rs
+++ src/linking.rs
@@ -1,15 +1,30 @@
-use std::{ffi::OsStr, path::Path};
+use std::{
+    ffi::{OsStr, OsString},
+    path::Path,
+};
+
+fn cc_file(name: &str) -> OsString {
+    let out = std::process::Command::new("cc")
+        .arg(format!("-print-file-name={}", name))
+        .output()
+        .unwrap();
+    OsString::from(String::from_utf8(out.stdout).unwrap().trim())
+}
 
 pub fn link_file(object: &Path, out: &Path) {
+    let crt1 = cc_file("crt1.o");
+    let crti = cc_file("crti.o");
+    let crtn = cc_file("crtn.o");
+
     let mut linker = std::process::Command::new("ld")
         .args([
             OsStr::new("-o"),
             out.as_os_str(),
             object.as_os_str(),
             OsStr::new("-lc"),
-            OsStr::new("/usr/lib/crti.o"),
-            OsStr::new("/usr/lib/crt1.o"),
-            OsStr::new("/usr/lib/crtn.o"),
+            &crti,
+            &crt1,
+            &crtn,
         ])
         .spawn()
         .unwrap();
